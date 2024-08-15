88a8bfad5b7b94bafb39b43cb77490ab8df804f7
Mathis-Mathis <mathisu.mousli@gmail.com>
2024-08-15 07:32:35 +0200

    Reset main branch

--- frontend/src/services/muslesService.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export default class MusclesService {
-    static async getMuscles(userName) {
-        return axios.get('http://localhost:5000/api/v1/users/{userName}');
-    }
-}
